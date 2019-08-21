3310e2f4de62d4f0bb270bf9f3f60eb0f01b5524
jrking365 <nigoumijeanroger@gmail.com>
2019-08-21 06:33:26 -0400

    connecting to DB

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 // DB Config
 const db = require('./config/keys').mongoURI
 
+//Connect to  mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));
+
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
