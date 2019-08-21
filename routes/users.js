07f12ee7219e6e6550c7cdf3ee4940e4a07699ff
jrking365 <nigoumijeanroger@gmail.com>
2019-08-21 07:01:35 -0400

    working on the register form

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,37 @@ router.get('/login', (req, res) => res.render('login'));
 //register page
 router.get('/register', (req, res) => res.render('register'));
 
+//Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill  in all fields ' })
+    }
+
+    // Check Password match 
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    //Check password length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password should be atleast 6 characters' });
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('pass');
+    }
+})
+
 module.exports = router;
