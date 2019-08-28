bd5a92f0d72e838aca3ec2a3166afeb72f742853
jrking365 <nigoumijeanroger@gmail.com>
2019-08-28 06:32:04 -0400

    working on auth connection and logout

--- routes/users.js
+++ routes/users.js
@@ -89,4 +89,11 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+//logout handle
+router.get('/logout', (req, res, next) => {
+    req.logOut();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+})
+
 module.exports = router;
